34ea3a991928cc1844cee995ac43ef9dc7e1e5cf
bboston2008 <104662326+bboston2008@users.noreply.github.com>
2022-06-15 21:03:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+// Variables -----
+
+// ----- Create
+const inputLength = () => userInput.value.length;
+const userInput = document.querySelector('#userInput');
+const button = document.querySelector('#enter');
+
+// ----- Delete
+const ul = document.querySelector('ul');
+const checkbox = document.querySelectorAll('input[type=checkbox]');
+const delButton = document.querySelector('#erase');
+
+// Create list item ------
+
+const addButton = () => {
+    if (inputLength() > 0) { newItem() };
+};
+
+const addEnter = () => {
+    if (inputLength() > 0 && event.keyCode === 13) { newItem() };
+}
+
+const newItem = () => {
+    let li = document.createElement('li');
+    let div = document.createElement('div');
+    let makeCheckbox = document.createElement('input');
+    makeCheckbox.type = "checkbox";
+    li.appendChild(makeCheckbox);
+    makeCheckbox.classList.add('box');
+    li.appendChild(document.createTextNode(userInput.value));
+    ul.appendChild(div);
+    div.appendChild(li);
+    div.classList.add('listItemDiv');
+    userInput.value = '';
+}
+
+// Strike items ------
+
+const lineThru = (event) => event.target.closest('input').checked ?
+    event.target.closest('li').classList.add('marked') :
+    event.target.closest('li').classList.remove('marked');
+
+
+
+// Remove selected items -------
+
+function remove() {
+    var checked = document.querySelectorAll(".box:checked");
+    checked.forEach((elem) => {
+        elem.parentElement.style.display = "none";
+    })
+}
+
+// Events ------
+
+delButton.addEventListener('click', remove);
+button.addEventListener('click', addButton);
+userInput.addEventListener('keypress', addEnter);
+ul.addEventListener('click', lineThru)
