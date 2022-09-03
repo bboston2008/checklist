c140e202bff72045d3d037ee78c26f1dc1757f7a
bboston2008 <104662326+bboston2008@users.noreply.github.com>
2022-09-02 20:59:03 -0500

    Added the ability to select two different lists plus a moveable floating input

--- script.js
+++ script.js
@@ -4,9 +4,48 @@ const enter = document.querySelector('#enter-items'),
       ul = document.querySelector('ul'),
       delButton = document.querySelector('#erase'),
       active = document.getElementById('act-list'),
-      finished = document.getElementById('fin-list');
+      activeTwo = document.getElementById('act-list-2'),
+      finished = document.getElementById('fin-list'),
+      response = document.getElementById('category').value,
+      buttonBox = document.getElementById('button-box');
+
+//Move Div ------
+var mousePosition;
+var offset = [0,0];
+var isDown = false;
+
+buttonBox.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        buttonBox.offsetLeft - e.clientX,
+        buttonBox.offsetTop - e.clientY
+    ];
+}, true);
+
+buttonBox.addEventListener('mouseup', function() {
+    isDown = false;
+}, true);
+
+buttonBox.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown) {
+        mousePosition = {
+    
+            x : event.clientX,
+            y : event.clientY
+    
+        };
+        buttonBox.style.left = (mousePosition.x + offset[0]) + 'px';
+        buttonBox.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+}, true);   
 
 // Create list item ------
+
+// const whichList = () => {
+    
+//     console.log(response);
+// }
 
 const addButton = () => {
     if (inputLength() > 0) { newItem() };
@@ -17,15 +56,22 @@ const addEnter = () => {
 }
 
 const newItem = () => {
-    let li = document.createElement('li');
-    let button = document.createElement('button');
+    let li = document.createElement('li'),
+        button = document.createElement('button'),
+        response = document.getElementById('category').value;
     li.classList.add('item');
     button.innerHTML = 'DONE';
     button.onclick = moveToDo;
     button.classList.add('done-button');
     li.appendChild(button);
     li.appendChild(document.createTextNode(userInput.value));
-    active.appendChild(li);
+    if (response === 'Grocery1') {
+        li.classList.add('g');
+        active.appendChild(li);
+    } else if (response === 'ToDo1') {
+        li.classList.add('t');
+        activeTwo.appendChild(li);
+    }
     userInput.value = '';
 }
 
@@ -46,13 +92,19 @@ const moveToDo = (evt) => {
 const undoToDo = (evt) => {
     let button = document.createElement('button'),
         btn = evt.target,
-        li = btn.closest('li');
+        li = btn.closest('li'),
+        response = document.getElementById('category').value;
     btn.remove();
     button.innerHTML = 'DONE';
     button.onclick = moveToDo;
     button.classList.add('done-button');
     li.insertBefore(button, li.firstChild);
-    active.appendChild(li).classList.remove('marked');
+    if (li.classList.contains('g')) {
+        active.appendChild(li).classList.remove('marked')
+    } else  if (li.classList.contains('t')) {
+        activeTwo.appendChild(li).classList.remove('marked')
+    }
+
 }
 
 // Clear Completed List -------
